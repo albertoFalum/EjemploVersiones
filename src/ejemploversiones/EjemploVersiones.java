78d41b21c8d6b008c1555a6443b07866a850fa2a
albertoFalum <alberto.alum@outlook.com.ar>
2023-08-07 17:37:18 -0300

    Elimine algunas cosas

--- src/ejemploversiones/EjemploVersiones.java
+++ src/ejemploversiones/EjemploVersiones.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package ejemploversiones;
 
 /**
@@ -10,11 +7,8 @@ package ejemploversiones;
  */
 public class EjemploVersiones {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+       
     }
     
 }
