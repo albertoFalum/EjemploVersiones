4c107cf05995e148f67a5862d87f3f56f855f461
albertoFalum <alberto.alum@outlook.com.ar>
2023-08-07 19:32:45 -0300

    Update Cliente
    Agregue atributos

--- src/ejemploversiones/entidades/Cliente.java
+++ src/ejemploversiones/entidades/Cliente.java
@@ -5,5 +5,29 @@ package ejemploversiones.entidades;
  * @author BETO
  */
 public class Cliente {
+    private String nombre;
+    private int DNI;
+
+    public Cliente(String nombre, int DNI) {
+        this.nombre = nombre;
+        this.DNI = DNI;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getDNI() {
+        return DNI;
+    }
+
+    public void setDNI(int DNI) {
+        this.DNI = DNI;
+    }
+    
     
 }
