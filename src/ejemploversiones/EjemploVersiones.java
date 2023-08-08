2211ea422816a4439e1ed84d3718bc4b18d46b6d
albertoFalum <alberto.alum@outlook.com.ar>
2023-08-08 15:56:38 -0300

    Creacion de Producto
    Cree un producto y agregue constructor y setters y getters en la clase Producto

--- src/ejemploversiones/EjemploVersiones.java
+++ src/ejemploversiones/EjemploVersiones.java
@@ -2,6 +2,7 @@
 package ejemploversiones;
 
 import ejemploversiones.entidades.Cliente;
+import ejemploversiones.entidades.Producto;
 
 /**
  *
@@ -12,6 +13,8 @@ public class EjemploVersiones {
     public static void main(String[] args) {
        Cliente cliente=new Cliente("Alberto", 4222222);
        Cliente c2=new Cliente("Sebas",12345678);
+       
+       Producto producto=new Producto("Aceite", 15, 22.50, 6);
     }
     
 }
